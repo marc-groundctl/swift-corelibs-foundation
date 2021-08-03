a9b2a20c66e2b8c3a6637f268c1379b23d73a2c7
Marc Aldorasi <marc@groundctl.com>
2021-08-03 15:21:02 -0400

    Use size_t for malloc arguments

--- CoreFoundation/URL.subproj/CFURLComponents_URIParser.c
+++ CoreFoundation/URL.subproj/CFURLComponents_URIParser.c
@@ -280,7 +280,7 @@ CF_EXPORT CFStringRef _CFStringCreateByAddingPercentEncodingWithAllowedCharacter
             }
             else {
                 // not big enough? malloc it.
-                CFIndex mallocSize;
+                size_t mallocSize;
                 if ( _CFMultiplyBufferSizeWithoutOverflow(maxBufferSize, 4, &mallocSize) ) {
                     if (mallocSize >= 0) {
                         inBuf = (UInt8 *)malloc(mallocSize);
@@ -457,7 +457,7 @@ CF_EXPORT CFStringRef _CFStringCreateByRemovingPercentEncoding(CFAllocatorRef al
             }
             else {
                 // not big enough? malloc it.
-                CFIndex mallocSize;
+                size_t mallocSize;
                 if ( _CFMultiplyBufferSizeWithoutOverflow(maxBufferSize, 2, &mallocSize) ) {
                     if (mallocSize >= 0) {
                         encodedBuf = (UInt8 *)malloc(mallocSize);
